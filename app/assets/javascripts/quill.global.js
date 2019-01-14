4e971e953be928dbf89f5624ca5695359365ab06
Rob Hill <robert.hill0122@gmail.com>
2019-01-13 17:33:36 -0700

    Change default options to include link and image

--- app/assets/javascripts/quill.global.js
+++ app/assets/javascripts/quill.global.js
@@ -12,7 +12,7 @@
                 [{ 'header': [1, 2, 3, false] }],
                 [{ 'color': [] }, { 'background': [] }],
                 ['bold', 'italic', 'underline', 'strike'],
-                ['blockquote', 'code-block'],
+                ['link', 'image'],
                 [{ 'list': 'ordered'}, { 'list': 'bullet' }],
                 [{ 'indent': '-1'}, { 'indent': '+1' }],
                 ['clean']
